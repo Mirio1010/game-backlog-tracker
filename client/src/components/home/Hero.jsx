a6fcd644ef91530117f7c8c899188ab19ed56a57
Miguelangel Ortega <miguelangelortega2021@gmail.com>
2026-04-14 14:02:37 -0400

    add minimal styling to page

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -3,22 +3,22 @@ import HomeImage from "./HomeImage.jsx";
 import LoginBtn from "../ui/LoginBtn.jsx";
 import SignupBtn from "../ui/SignupBtn.jsx";
 import Header from "../layout/Header.jsx";
-
+import "../../styles/hero.css";
 const Hero = () => {
   return (
-    <>
+    <div className="Hero">
       <Header />
       <h1>Destroy your game backlog one step at a time</h1>
       <p>
         Welcome to Game Backlog Tracker, the ultimate tool for gamers who want
-        to conquer their game backlog and stay organized. With our intuitive
+        to conquer their game backlog and stay organized. With an intuitive
         interface and powerful features, you can easily manage your game
         collection, track your progress, and discover new games to play.
       </p>
       <HomeImage />
       <LoginBtn />
       <SignupBtn />
-    </>
+    </div>
   );
 }
 
--- client/src/components/home/HomeImage.jsx
+++ client/src/components/home/HomeImage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import imageSrc from "../../assets/homepage/video_game_collection.png";
+import "../../styles/hero.css";
 const HomeImage = () => {
   return (
-    <div>
-      <img src={imageSrc} alt="Video Game Collection" />
+    <div >
+      <img className='Hero-image' src={imageSrc} alt="Video Game Collection" />
     </div>
   );
 }
