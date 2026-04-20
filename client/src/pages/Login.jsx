da6cb3d06758fe9df23b57f2e85ba6e861510faf
Mirio1010 <miguelangelortega2021@gmail.com>
2026-04-19 21:12:17 -0400

    Update backend dev port to 5001

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -55,7 +55,7 @@ const Form = () => {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://localhost:5001/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
