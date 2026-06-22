085fd3e3d25f25f024599ffc4099535dda3e41d3
Mirio1010 <miguelangelortega2021@gmail.com>
2026-06-21 22:41:18 -0400

    fix gameVideosApi service

--- client/src/api/gameVideosApi.js
+++ client/src/api/gameVideosApi.js
@@ -3,27 +3,29 @@ const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 export const getGameMovies = async (rawgId) => {
   if (!rawgId) return [];
 
-  const response = await fetch(
-    `${API_BASE_URL}/api/rawg/games/${rawgId}/videos`,
-  );
+  const response = await fetch(`${API_BASE_URL}/rawg/games/${rawgId}/videos`);
+
+  const data = await response.json();
 
   if (!response.ok) {
-    throw new Error("Failed to fetch gameplay videos");
+    throw new Error(data.message || "Failed to fetch gameplay videos");
   }
 
-  return response.json();
+  return data;
 };
 
 export const getGameScreenshots = async (rawgId) => {
   if (!rawgId) return [];
 
   const response = await fetch(
-    `${API_BASE_URL}/api/rawg/games/${rawgId}/screenshots`,
+    `${API_BASE_URL}/rawg/games/${rawgId}/screenshots`,
   );
 
+  const data = await response.json();
+
   if (!response.ok) {
-    throw new Error("Failed to fetch game screenshots");
+    throw new Error(data.message || "Failed to fetch game screenshots");
   }
 
-  return response.json();
+  return data;
 };
